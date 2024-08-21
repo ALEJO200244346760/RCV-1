c01664cefdd7325aa940b2e92305a2955df1882d
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2024-08-21 19:31:41 -0300

    dale ne

---
+++ rcv_Backend/src/main/java/com/backend/rcv/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.backend.rcv.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll()  // Permitir todas las peticiones sin autenticación
+                )
+                .csrf(csrf -> csrf.disable());  // Deshabilitar la protección CSRF
+
+        return http.build();
+    }
+}
