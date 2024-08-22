36e16b0417db52bd0bf0a46316a6999cd7be6c58
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2024-08-22 20:59:20 -0300

    daaa

--- riesgo-cardiovascular/src/axiosConfig.js
+++ riesgo-cardiovascular/src/axiosConfig.js
@@ -1,8 +1,7 @@
-// src/services/axiosConfig.js
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://rcv-production.up.railway.app', // Asegúrate de que esta URL sea correcta
+    baseURL: 'https://rcv-production.up.railway.app',
     timeout: 5000,
     headers: {'Content-Type': 'application/json'}
 });
