ca9082c9c24c4fae6d92cecbf5a6b1bbf85c7552
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2024-08-28 03:02:34 -0300

    actualizacion 28/08

--- riesgo-cardiovascular/src/components/Header.jsx
+++ riesgo-cardiovascular/src/components/Header.jsx
@@ -7,8 +7,8 @@ const Header = () => {
     <header className="bg-blue-600 text-white py-4 px-6 flex justify-between items-center">
       <h1 className="text-2xl font-bold">Riesgo Cardiovascular</h1>
       <nav className="space-x-4">
-        <Link to="/tomarPresion" className="hover:text-gray-300">Indicaciones</Link>
-        <Link to="/formulario" className="hover:text-gray-300">Formulario</Link>
+        <Link to="/tomarPresion" className="hover:text-gray-300">Diagnostico</Link>
+        <Link to="/formulario" className="hover:text-gray-300">RCV</Link>
         <Link to="/estadisticas" className="hover:text-gray-300">Estadísticas</Link>
         <Link to="/Login" className="hover:text-gray-300">Iniciar Sesión</Link>
       </nav>
