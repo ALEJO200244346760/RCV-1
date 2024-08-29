2a12ba8c2e207e1f17da0929d8b6f7eff2b30e76
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2024-08-29 18:23:02 -0300

    actualizacion 28/08

--- rcv_Backend/src/main/java/com/backend/rcv/config/WebConfig.java
+++ rcv_Backend/src/main/java/com/backend/rcv/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("https://ercv-oms.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
