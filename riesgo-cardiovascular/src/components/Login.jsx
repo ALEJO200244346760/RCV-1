c356e9548d157e84cff791065c8d1d1cc96dade0
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2024-08-29 12:21:26 -0300

    actualizacion 28/08

--- riesgo-cardiovascular/src/components/Login.jsx
+++ riesgo-cardiovascular/src/components/Login.jsx
@@ -1,23 +1,25 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../../context/useAuth'; // Asegúrate de la ruta correcta
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const { login } = useAuth(); // Usa el hook de autenticación para obtener la función login
+  const navigate = useNavigate();
 
   const handleLogin = async (event) => {
     event.preventDefault();
     try {
-      const response = await axios.post('/login', {
-        email,
-        password,
-      });
-      localStorage.setItem('token', response.data.token);
-      console.log('Login exitoso:', response.data);
+      const response = await axios.post('/login', { email, password });
+      const token = response.data.token;
+      login(token); // Almacena el token usando la función login del contexto
+      navigate('/'); // Redirige al home o a la página deseada
     } catch (error) {
       console.error('Error en el login:', error.response?.data);
-      alert('Error en el inicio de sesión. Verifica tus credenciales.');
+      setError('Error en el inicio de sesión. Verifica tus credenciales.');
     }
   };
 
@@ -25,6 +27,7 @@ const Login = () => {
     <div className="flex justify-center items-center h-screen bg-gray-100">
       <form onSubmit={handleLogin} className="bg-white p-8 rounded shadow-md w-full max-w-sm">
         <h2 className="text-2xl mb-6 text-center font-bold">Iniciar Sesión</h2>
+        {error && <p className="text-red-500 mb-4">{error}</p>}
         <div className="mb-4">
           <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
           <input
