93781ec2c7c853f7b95cd2230dba6ec5dc4a4e66
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2024-08-29 09:08:55 -0300

    actualizacion 28/08

--- riesgo-cardiovascular/src/components/Header.jsx
+++ riesgo-cardiovascular/src/components/Header.jsx
@@ -5,7 +5,9 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header className="bg-red-600 text-white py-4 px-6 flex justify-between items-center">
-      <h1 className="text-2xl font-bold">RCV</h1>
+      <Link to="/formulario" className="text-2xl font-bold hover:text-gray-300">
+        <h1>RCV</h1>
+      </Link>
       <nav className="space-x-4">
         <Link to="/tomarPresion" className="hover:text-gray-300">Diagnostico</Link>
         <Link to="/formulario" className="hover:text-gray-300">RCV</Link>
