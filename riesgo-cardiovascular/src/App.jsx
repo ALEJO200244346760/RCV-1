28621a3269c4e9be7d3fc6a4599e09b94ad6bce4
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2024-08-29 11:46:41 -0300

    actualizacion 28/08

--- riesgo-cardiovascular/src/App.jsx
+++ riesgo-cardiovascular/src/App.jsx
@@ -5,7 +5,7 @@ import Estadisticas from './components/Estadisticas';
 import Formulario from './components/Formulario';
 import EditarPaciente from './components/EditarPaciente';
 import TomarPresion from './components/tomarPresion';
-import Login from './components/login';
+import Login from './components/Login';
 import Register from './components/Register'; // Importa el componente Register
 
 
