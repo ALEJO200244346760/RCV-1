69af3d224d1b755af9dd44b0563af8d9622f40c8
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2024-08-29 19:24:27 -0300

    a

--- riesgo-cardiovascular/src/App.jsx
+++ riesgo-cardiovascular/src/App.jsx
@@ -4,27 +4,38 @@ import Header from './components/Header';
 import Estadisticas from './components/Estadisticas';
 import Formulario from './components/Formulario';
 import EditarPaciente from './components/EditarPaciente';
-import TomarPresion from './components/tomarPresion';
+import TomarPresion from './components/TomarPresion';
 import Login from './components/Login';
 import Register from './components/Register'; // Importa el componente Register
-
+import RoleProtectedRoute from './components/RoleProtectedRoute';
 
 function App() {
   return (
     <Router>
-      <Header />  {/* Muestra el Header en todas las páginas */}
+      <Header />
       <Routes>
-        <Route path="/" element={<Formulario />} /> {/* Página principal */}
+        <Route path="/" element={<Formulario />} />
         <Route path="/tomarPresion" element={<TomarPresion />} />
-        <Route path="/formulario" element={<Formulario />} />
-        <Route path="/estadisticas" element={<Estadisticas />} />
-        <Route path="/editar-paciente/:id" element={<EditarPaciente />} />
+        <Route
+          path="/estadisticas"
+          element={<Estadisticas />}
+              allowedRoles={['CARDIOLOGO']}
+            />
+        <Route
+          path="/editar-paciente/:id"
+          element={
+            <RoleProtectedRoute
+              element={<EditarPaciente />}
+              allowedRoles={['CARDIOLOGO']}
+            />
+          }
+        />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/register" element={<Register />} /> {/* Añade la ruta para el registro */}
+        <Route path="*" element={<Navigate to="/" />} /> {/* Ruta 404 */}
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
--- riesgo-cardiovascular/src/hooks/useAuthToken.jsx
+++
@@ -1,8 +0,0 @@
-import useAuth from '../context/useAuth'; // Ensure this path is correct
-
-const useAuthToken = () => {
-    const { token } = useAuth();
-    return token;
-};
-
-export default useAuthToken;
