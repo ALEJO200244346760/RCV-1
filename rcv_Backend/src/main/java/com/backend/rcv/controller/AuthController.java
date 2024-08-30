eb0452d18d621730e9010601500958a0b3b01a37
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2024-08-30 00:21:50 -0300

    aaa

--- rcv_Backend/src/main/java/com/backend/rcv/controller/AuthController.java
+++ rcv_Backend/src/main/java/com/backend/rcv/controller/AuthController.java
@@ -8,7 +8,6 @@ import com.backend.rcv.jwt.util.JwtUtil;
 import com.backend.rcv.model.Usuario;
 import com.backend.rcv.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -16,7 +15,6 @@ import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +23,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin(origins = "*")
 public class AuthController {
 
     @Autowired
@@ -40,34 +37,27 @@ public class AuthController {
     private UsuarioService usuarioService;
 
     @PostMapping("/login")
-    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) {
-        try {
-            authenticate(authenticationRequest.getEmail(), authenticationRequest.getPassword());
-            final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getEmail());
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
+        authenticate(authenticationRequest.getEmail(), authenticationRequest.getPassword());
 
-            Usuario usuario = usuarioService.findByEmail(authenticationRequest.getEmail())
-                    .orElseThrow(() -> new Exception("User not found"));
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getEmail());
 
-            List<String> roles = userDetails.getAuthorities().stream()
-                    .map(authority -> authority.getAuthority())
-                    .collect(Collectors.toList());
+        Usuario usuario = usuarioService.findByEmail(authenticationRequest.getEmail())
+                .orElseThrow(() -> new Exception("User not found"));
 
-            final String token = jwtUtil.generateToken(userDetails, roles, usuario.getNombre(), usuario.getApellido());
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(authority -> authority.getAuthority())
+                .collect(Collectors.toList());
 
-            return ResponseEntity.ok(new JwtResponse(token));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred");
-        }
+        final String token = jwtUtil.generateToken(userDetails, roles, usuario.getNombre(), usuario.getApellido());
+
+        return ResponseEntity.ok(new JwtResponse(token));
     }
 
     @PostMapping("/register")
-    public ResponseEntity<?> saveUser(@RequestBody UsuarioDto usuario) {
-        try {
-            usuario.setRol("ENFERMERO");
-            return ResponseEntity.ok(userDetailsService.save(usuario));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred");
-        }
+    public ResponseEntity<?> saveUser(@RequestBody UsuarioDto usuario) throws Exception {
+        usuario.setRol("ENFERMERO");
+        return ResponseEntity.ok(userDetailsService.save(usuario));
     }
 
     private void authenticate(String email, String password) throws Exception {
@@ -79,4 +69,7 @@ public class AuthController {
             throw new Exception("INVALID_CREDENTIALS", e);
         }
     }
+
+
 }
+
