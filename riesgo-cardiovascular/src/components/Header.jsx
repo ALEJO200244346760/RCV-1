5cfb363fb223b33948197350ba7abb3b23b1f3e2
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2024-09-04 15:07:06 -0300

    asd

--- riesgo-cardiovascular/src/components/Header.jsx
+++ riesgo-cardiovascular/src/components/Header.jsx
@@ -1,18 +1,33 @@
 // src/components/Header.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'; // Necesitarás instalar react-icons
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <header className="bg-red-600 text-white py-4 px-6 flex justify-between items-center">
+    <header className="bg-red-600 text-white py-4 px-6 flex justify-between items-center relative">
       <Link to="/formulario" className="text-2xl font-bold hover:text-gray-300">
         <h1>RCV</h1>
       </Link>
-      <nav className="space-x-4">
-        <Link to="/tomarPresion" className="hover:text-gray-300">Diagnostico</Link>
-        <Link to="/formulario" className="hover:text-gray-300">RCV</Link>
-        <Link to="/estadisticas" className="hover:text-gray-300">Estadísticas</Link>
-        <Link to="/login" className="hover:text-gray-300">Iniciar Sesión</Link>
+
+      <button 
+        onClick={toggleMenu} 
+        className="lg:hidden flex items-center text-white"
+      >
+        {isMenuOpen ? <AiOutlineClose size={24} /> : <AiOutlineMenu size={24} />}
+      </button>
+
+      <nav className={`lg:flex lg:space-x-4 ${isMenuOpen ? 'block' : 'hidden'} lg:block`}>
+        <Link to="/tomarPresion" className="block lg:inline-block hover:text-gray-300">Diagnóstico</Link>
+        <Link to="/formulario" className="block lg:inline-block hover:text-gray-300">RCV</Link>
+        <Link to="/estadisticas" className="block lg:inline-block hover:text-gray-300">Estadísticas</Link>
+        <Link to="/login" className="block lg:inline-block hover:text-gray-300">Iniciar Sesión</Link>
       </nav>
     </header>
   );
