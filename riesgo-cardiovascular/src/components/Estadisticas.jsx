d7957bd308a08ddcb2aa6232b7509a6a6c103d93
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2024-09-09 15:46:09 -0300

    asd

--- riesgo-cardiovascular/src/components/Estadisticas.jsx
+++ riesgo-cardiovascular/src/components/Estadisticas.jsx
@@ -383,6 +383,7 @@ function Estadisticas() {
                     </span>
                   </td>
                   <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-500">{paciente.ubicacion}</td>
+                  <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-500">{paciente.imc}</td>
                   <td className="px-4 py-4 whitespace-nowrap text-right text-sm font-medium">
                     <button
                       onClick={() => editarPaciente(paciente.id)}
