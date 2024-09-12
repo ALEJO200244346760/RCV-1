52a47733568dda17004806319595700f691a75fc
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2024-09-12 15:34:54 -0300

    asd

--- riesgo-cardiovascular/src/index.js
+++ riesgo-cardiovascular/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Actualiza la importación
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { AuthProvider } from './context/AuthContext'; // Asegúrate de importar correctamente
 
-// Crear la raíz del DOM
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// Renderizar la aplicación
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
