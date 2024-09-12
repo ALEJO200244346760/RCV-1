327defce974ee0778ac41284b7b3f5c358e3a9f5
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2024-09-12 17:59:35 -0300

    asd

--- riesgo-cardiovascular/src/services/userService.jsx
+++ riesgo-cardiovascular/src/services/userService.jsx
@@ -1,23 +1,43 @@
-// src/services/userService.js
-
+// userService.js
 export const getUsers = async () => {
-  const response = await fetch('/usuario');
-  if (!response.ok) {
-    throw new Error('Error al obtener usuarios');
+  try {
+    const response = await fetch('https://rcv-production.up.railway.app/usuario'); // Actualiza con tu URL
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const contentType = response.headers.get('content-type');
+    if (contentType && contentType.includes('application/json')) {
+      const data = await response.json();
+      return data;
+    } else {
+      throw new Error('Expected JSON response');
+    }
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    throw error; // Lanzar error para que pueda ser manejado por el componente
   }
-  return response.json();
 };
 
 export const updateUserRoleAndLocation = async (userId, updates) => {
-  const response = await fetch(`/usuario/${userId}`, {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(updates),
-  });
-  if (!response.ok) {
-    throw new Error('Error al actualizar usuario');
+  try {
+    const response = await fetch(`https://rcv-production.up.railway.app/usuario/${userId}`, { // Actualiza con tu URL
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updates),
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const contentType = response.headers.get('content-type');
+    if (contentType && contentType.includes('application/json')) {
+      return await response.json();
+    } else {
+      throw new Error('Expected JSON response');
+    }
+  } catch (error) {
+    console.error('Error updating user role and location:', error);
+    throw error; // Lanzar error para que pueda ser manejado por el componente
   }
-  return response.json();
 };
