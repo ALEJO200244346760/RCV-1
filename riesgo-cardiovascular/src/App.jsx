5e2aed237dabd40d1926de3e93f5cf70d2feeab7
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2024-09-12 15:27:20 -0300

    asd

--- riesgo-cardiovascular/src/App.jsx
+++ riesgo-cardiovascular/src/App.jsx
@@ -1,33 +1,41 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext'; // Asegúrate de que la ruta sea correcta
 import Header from './components/Header';
 import Estadisticas from './components/Estadisticas';
 import Formulario from './components/Formulario';
 import EditarPaciente from './components/EditarPaciente';
-import TomarPresion from './components/tomarPresion';
+import TomarPresion from './components/TomarPresion';
 import Login from './components/Login';
 import Register from './components/Register'; // Importa el componente Register
-
+import RoleProtectedRoute from './components/RoleProtectedRoute';
 
 function App() {
   return (
-    <AuthProvider>
     <Router>
-      <Header />  {/* Muestra el Header en todas las páginas */}
+      <Header />
       <Routes>
-        <Route path="/" element={<Formulario />} /> {/* Página principal */}
+        <Route path="/" element={<Formulario />} />
         <Route path="/tomarPresion" element={<TomarPresion />} />
-        <Route path="/formulario" element={<Formulario />} />
-        <Route path="/estadisticas" element={<Estadisticas />} />
-        <Route path="/editar-paciente/:id" element={<EditarPaciente />} />
+        <Route
+          path="/estadisticas"
+          element={<Estadisticas />}
+              allowedRoles={['CARDIOLOGO']}
+            />
+        <Route
+          path="/editar-paciente/:id"
+          element={
+            <RoleProtectedRoute
+              element={<EditarPaciente />}
+              allowedRoles={['CARDIOLOGO']}
+            />
+          }
+        />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/register" element={<Register />} /> {/* Añade la ruta para el registro */}
+        <Route path="*" element={<Navigate to="/" />} /> {/* Ruta 404 */}
       </Routes>
     </Router>
-    </AuthProvider>
   );
 }
 
 export default App;
-
